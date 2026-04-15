1c06893deaa899d3730838e1a72092b75bb4e63e
elite <mitali777770@gmail.com>
2026-04-15 11:37:38 +0900

    feat: learn python boolean

--- primitive.py
+++ primitive.py
@@ -27,3 +27,19 @@ result = course.replace("FullStack", "MasterClass") #immutable
 print(f"the result (4): {result}")
 
 print("======= boolean ==========")
+# functions > type() input() bool() int()(numberni stringga) str()(stringni numberga)
+y = input("Give your value for y: ")
+print("y:", y)
+
+result = y.isnumeric() #number kiritildimi
+print(f"the input value is numeric: {result}")
+
+#Truthy vs Falsy value
+#Truthy: True 100 -100 "MIT"
+#Falsy: False 0 "" None
+
+test_falsy = "" or False or None or 0 or 100
+print("The test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("The test_truthy:", bool(test_truthy))
