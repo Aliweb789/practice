9feb6b3c026e869dcef4c3473502bca435dd699f
elite <mitali777770@gmail.com>
2026-04-15 12:15:58 +0900

    fix: learn keyword & default arguments

--- functions.py
+++ functions.py
@@ -12,6 +12,8 @@ print("====== DEFINE (parameter) vs CALL (argument)=======")
 # Instead of block {} in JAVA, Python uses indentation!
 
 # DEFINE - build(parameter)
+
+
 def greet(a):
     # pass (hech narsa qilma)
     print(f"How do you do, {a}")
@@ -31,3 +33,19 @@ print("result1:", result1)  # --> console da none chiqadi
 
 result2 = greeting('Justin')
 print("result2:", result2)
+
+print("====== Keyword & default arguments =======")
+
+# DEFINE
+
+
+def give_greet(name, age = 22):
+    print("give_greet is executed")
+    return f"Hi {name}, you are {age} years old"
+
+#CALL
+result3 = give_greet(name = 'Justin', age = 28) #keyword arguments
+print("result3:", result3)
+
+result4 = give_greet("John") #default argument(age)
+print("result3:", result4)
