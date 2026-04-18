2f6db25a6023b1018c38cdb504fb84cc9d312736
elite <mitali777770@gmail.com>
2026-04-18 21:42:30 +0900

    fix: python special/magic methods

--- class.about.py
+++ class.about.py
@@ -1,7 +1,7 @@
 ''' CLASS
         (1) What is class
         (2) ordinary vs static properties
-        (3) special methods
+        (3) special/magic methods
 '''
 
 print("========= What is class ==========")
@@ -43,9 +43,55 @@ person1.introduce()
 person2.say_age()
 
 print("========= ordinary vs static properties ==========")
-#static state --> clasni ozzi bilan keladi, object bilan emas
+# static state --> clasni ozzi bilan keladi, object bilan emas
 new_message = Person.message
 print("new_message:", new_message)
 
 # static ,ethod
 Person.explain()
+
+print("========= special/magic methods ==========")
+# Python's most common special methods are below:
+# __init__ __new__ __str__ __call__ __getitem__ __eq__ __len__ ...
+
+
+class Car():
+    # state
+    description = "This class makes cars"
+
+    # constructor
+    def __new__(cls, *args): #__init__ methodi ishga tushishi uchun __new__ birinchi ishga tushadi, kop hollarda korinmaydi(lekin automotic yozilgan boladi)
+        print("*__new__*")
+        return super().__new__(cls) #doimo yozilishi kerak
+
+
+    def __init__(self, name, year):
+        self.name = name
+        self.year = year
+
+    # method
+    def start_engine(self):
+        print(f"the {self.name} started engine")
+
+    def stop_engine(self):
+        print(f"the {self.name} stopped engine")
+
+    def __str__(self): #classni ozini print qilganda chiqaradigan malumotni yozish
+        return f"the car.name: {self.name} was produced in {self.year} year!"
+
+    def __call__(self): #classlarni function larga oxshab chaqirtirishda
+        print("Object called as function")
+        return True
+
+
+my_car = Car("Ferrari", 2025)
+my_car.start_engine()
+my_car.stop_engine()
+
+
+print("____________")
+your_car = Car("Tayota", 2026)
+print(your_car)
+#your_car() #looks like function
+response = your_car()
+print("response:", response)
