89057cbf76c70c45a6aacae0050904c9df0d0928
Ali <mitali777770@gmail.com>
2026-04-21 23:57:33 +0900

    task: c-task is solved

--- train.js
+++ train.js
@@ -1,7 +1,19 @@
 
-//====================================================================================
+/* C-TASK:
+    Savol: Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+    Masalan: checkContent("mitgroup", "gmtiprou") return qiladi true;
+*/
 
-<<<<<<< HEAD
+//masalaning yechimi:
+const checkContent = function (text1, text2) {
+    let count = 0;
+    for (const letter of text1) {
+        if (text2.includes(letter)) count++
+    }
+    bool_result = (count === text1.length) && (count === text2.length);
+    console.log("solution is : " + bool_result);
+}
+checkContent("mitgroup", "gmtiprou")
 
 /* B-TASK:
     Savol: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
@@ -19,13 +31,11 @@ MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 //     console.log("solution is : " + count);
 // }
 
-// countDigits("ad2a54y79we87t450sfgb9") 
+// countDigits("ad2a54y79we87t450sfgb9")
 
 
 
 
-=======
->>>>>>> b5fa1c8e5008a3060ca78d827208b7396bb5a2c5
 /* A-TASK:
     Savol: Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi sozdan qatnashga sonini return qilishi kerak boladi.
 MASALAN countLetter("e", "engineer") 3ni return qiladi.
@@ -48,24 +58,3 @@ countLetter("e", "engineer") //==>solution: 3
 
 */
 
-<<<<<<< HEAD
-=======
-
-/* B-TASK:
-    Savol: Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
-
-MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
-*/
-
-//masalaning yechimi:
-const countDigits = function(text) {
-    let count = 0;
-    for (const isNumber of text) {
-        // console.log(typeof isNumber);
-        if(isNumber % 1 == 0) count++
-    }
-    console.log("solution is : " + count);
-}
-
-countDigits("ad2a54y79we87t450sfgb9")
->>>>>>> b5fa1c8e5008a3060ca78d827208b7396bb5a2c5
