53f44ed85f0c00c76428f8ba14ca4b38acbd386f
Ali <mitali777770@gmail.com>
2026-04-21 22:30:45 +0900

    feat: learn conditions and logical operators

--- opercon.py
+++ opercon.py
@@ -34,3 +34,46 @@ print(id(c), id(d), id(e)) #id() - obyektning unikal identifikatori
 # data = c is d #is -> referensiyani solishtiradi, c va d bir xil obyektga ishora qiladimi
 print("c is d:", c is d)
 print("c is e:", c is e)
+
+print("========= Conditions  =========")
+
+x = 5;
+#consitionlar truthy va falsy qiymatlarni tekshiradi
+if x > 50:
+    print("Case A")
+elif x > 10:
+    print("Case B")
+else:
+    print("Case C")
+    
+print("------")
+print("========= Logical Operators  =========")
+
+age = 18
+# person = None
+# if age >= 16:
+#     person = "adult"
+# else:
+#     person = "minor"
+    
+# print("person:", person)
+
+# ternary operator
+person = "adult" if age >= 18 else "minor"
+print("person:", person)
+
+print("------")
+
+is_student = True
+is_admin = False
+is_guest = True
+is_parent = True
+
+if not is_student:
+    print("Welcome here, Do you want to be a student!")
+elif is_admin:
+    print("Please go to this office!")
+elif is_guest or is_parent:
+    print("Waiting room is over there!")
+else:
+    print("Other cases")
