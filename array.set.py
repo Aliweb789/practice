3c41181294acd68ed2600f5099d036bf2a65e6cc
Ali <mitali777770@gmail.com>
2026-04-25 22:11:05 +0900

    fix: specific operators with set

--- array.set.py
+++ array.set.py
@@ -6,7 +6,7 @@
 from array import array
 print("========= Array =========")
 
-numbers = array("i", [1, 4, 7, 8, 41])
+numbers = array("i", [1, 4, 5, 7, 8, 41])
 print("numbers(1):", numbers)
 
 numbers.append(100)
@@ -19,3 +19,33 @@ print("numbers(3):" , numbers)
 
 del numbers[0:2]
 print("numbers(4):", numbers)
+
+print("========= Set =========")
+# {set} of unique collection without keeping order!
+new_numbers = array("i", [1, 4, 5, 7,  5, 7, 8, 4, 41])
+numb_set = set(new_numbers) #sonlar takrorlanmaydi
+
+print(f"numbs_set: {numb_set} and type {type(numb_set)}")
+
+
+numb_set.add(200)
+print("numbs_set(1):", numb_set)
+
+numb_set.add(7) #7 soni borligi sababli qoshmaydi
+print("numbs_set(1):", numb_set)
+
+
+print("========= Specific operators: | & - ^  =========")
+# | & - ^
+a = {10, 20, 50}
+b = {20, 40}
+
+result1 = a | b #union bir xil bolgan qiymatlarni olmay birlashtirib beradi
+result2 = a & b #intersection bir xil sonlar
+result3 = a - b #difference (qoshish ayirishga oxshaydi)
+result4 = a ^ b #symmetric difference (bir birida qaytalanmagan raqamlarni chiqarib beradi)
+
+print("result1:", result1)
+print("result2:", result2)
+print("result3:", result3)
+print("result4:", result4)
