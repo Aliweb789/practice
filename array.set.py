4cf718927f9a0b87f1018227692c09783645d922
Ali <mitali777770@gmail.com>
2026-04-25 21:43:17 +0900

    feat: learn python array
    wq

--- array.set.py
+++ array.set.py
@@ -4,6 +4,7 @@
     (3) Specific operators with set
 '''
 from array import array
+print("========= Array =========")
 
 numbers = array("i", [1, 4, 7, 8, 41])
 print("numbers(1):", numbers)
@@ -11,3 +12,10 @@ print("numbers(1):", numbers)
 numbers.append(100)
 numbers.insert(0,14)
 print("numbers(2):", numbers)
+
+numbers.remove(5)
+numbers.pop()
+print("numbers(3):" , numbers)
+
+del numbers[0:2]
+print("numbers(4):", numbers)
