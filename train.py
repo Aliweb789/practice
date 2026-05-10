ae9f80370a508589ebcf7f286fad83764b5f079f
Ali <mitali777770@gmail.com>
2026-05-11 08:41:49 +0900

    J-TASK is done

--- train.py
+++ train.py
@@ -1,15 +1,30 @@
+# K-TASK
+# Shunday function yozing, u string qabul qilsin va string ichidagi eng uzun sozni qaytarsin. Masalan: find_longest("I come from Uzbekistan")
+# return "Uzbekistan"
+def find_longest(str):
+    new_array = str.split()
+    max_leng = 0
+    for i in new_array:
+        if len(i) > max_leng:
+            max_leng = len(i)
+    for j in new_array:
+        if len(j) == max_leng:
+            print(j)
+find_longest("I come from Uzbekistan")
+
+
 # I-TASK
 #Shunday function tuzing, unga string argument pass bolsin. Function ushbu argumentdagi digitalni yangi stringda return qilsin 
 #MASALAN: get_digits("m14i1t") return qiladi "141"
 
-def get_digits(str):
-    new_str = ""
-    for letter in str:
-        if '0' <= letter <= '9':
-            new_str += letter
-    print(new_str)
+# def get_digits(str):
+#     new_str = ""
+#     for letter in str:
+#         if '0' <= letter <= '9':
+#             new_str += letter
+#     print(new_str)
 
-get_digits("m14i12t")
+# get_digits("m14i12t")
 
 
 
