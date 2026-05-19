1d2c59e32a02a6ce49ddfb3d1f3490249c4af7c3
Ali <mitali777770@gmail.com>
2026-05-19 16:21:07 +0900

    O-TASK is solved

--- train.py
+++ train.py
@@ -1,20 +1,33 @@
+# O-TASK (PYTHON)
+# Shunday function yozing, u har xil valuelardan iborat array qabul qilsin va List ichidagi sonlar yigindisini hisoblab chiqqan javobni qaytarsin.
+# MASALAN: calculate_summary([10, "10", {son: 10}, true, 35]) return 45
+
+def calculate_summary(arr):
+    numbers = []
+    sum = 0
+    for item in arr:
+        if isinstance(item, (int, float)) and not isinstance(item, (bool)):
+            numbers.append(item)
+    for i in numbers:
+        sum = sum + i
+    print(sum)
+
+
+calculate_summary([10, "13", {"son": 14}, True, 35])
+
+
 # M-TASK (PYTHON)
 # Shunday function yozing, u string qabul qilsin va string palindrom yani togri oqilganda ham, orqasidan oqilganda ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
 # MASALAN: palindrom_check("dad") return True;  palindrom_check("son") return False;
 
-def palindrom_check(str):
-    new_str = str[::-1]
-    if str == new_str:
-        print(True)
-    else:
-        print(False)
+# def palindrom_check(str):
+#     new_str = str[::-1]
+#     if str == new_str:
+#         print(True)
+#     else:
+#         print(False)
 
-palindrom_check("nonon")
-
-
-
-
-
+# palindrom_check("nonon")
 
 
 # K-TASK
@@ -33,8 +46,8 @@ palindrom_check("nonon")
 
 
 # I-TASK
-#Shunday function tuzing, unga string argument pass bolsin. Function ushbu argumentdagi digitalni yangi stringda return qilsin 
-#MASALAN: get_digits("m14i1t") return qiladi "141"
+# Shunday function tuzing, unga string argument pass bolsin. Function ushbu argumentdagi digitalni yangi stringda return qilsin
+# MASALAN: get_digits("m14i1t") return qiladi "141"
 
 # def get_digits(str):
 #     new_str = ""
@@ -46,7 +59,6 @@ palindrom_check("nonon")
 # get_digits("m14i12t")
 
 
-
 # G-Task
 # Shunday function tuzingki unga integerlardan iborat array pass bolsin va function bizga osha arrayning eng katta qiymatiga tegishli birinchi indexni qaytarsin.
 # MASALAN: get_highest_index([5, 21, 12, 21, 8]) return qiladi 1 sonini
@@ -56,4 +68,3 @@ palindrom_check("nonon")
 #     print(arr.index(result))
 
 # get_highest_index([5, 21, 22, 21, 8])
-
